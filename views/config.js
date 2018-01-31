bf483ba36d034d1b33741e979e03829747db6b45
Adam Więcek <adamw28@gmail.com>
2018-01-31 01:41:47 +0100

    Upgrade project

---
+++ views/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '1083383828864-0msedti31ven21hbvff346oafb6dmfop.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'abjh4dgRXh3Gqa2X3WARYNK6',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
